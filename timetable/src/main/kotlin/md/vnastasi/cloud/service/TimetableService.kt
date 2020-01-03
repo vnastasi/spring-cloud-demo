93f2acf29fae0cc96af298febe7996f43910ece5
Valentin Nastasi <pragmatiku@protonmail.com>
2020-01-03 15:45:27 +0100

    Replaced controllers with routers

--- timetable/src/main/kotlin/md/vnastasi/cloud/service/TimetableService.kt
+++ timetable/src/main/kotlin/md/vnastasi/cloud/service/TimetableService.kt
@@ -3,10 +3,13 @@ package md.vnastasi.cloud.service
 import kotlinx.coroutines.flow.Flow
 import md.vnastasi.cloud.endpoint.model.Arrival
 import md.vnastasi.cloud.endpoint.model.Departure
+import md.vnastasi.cloud.exception.ApiException
 
 interface TimetableService {
 
+    @Throws(ApiException::class)
     suspend fun getDepartures(code: String): Flow<Departure>
 
+    @Throws(ApiException::class)
     suspend fun getArrivals(code: String): Flow<Arrival>
 }
