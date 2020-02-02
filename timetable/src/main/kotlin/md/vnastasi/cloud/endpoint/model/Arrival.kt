da2765c08cc92daac31eb270a3b12da9d2c05765
Valentin Nastasi <pragmatiku@protonmail.com>
2020-02-02 15:52:18 +0100

    Unified date/time format in API response

--- timetable/src/main/kotlin/md/vnastasi/cloud/endpoint/model/Arrival.kt
+++ timetable/src/main/kotlin/md/vnastasi/cloud/endpoint/model/Arrival.kt
@@ -1,10 +1,13 @@
 package md.vnastasi.cloud.endpoint.model
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.OffsetDateTime
 
 data class Arrival(
     val origin: String,
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
     val plannedArrival: OffsetDateTime,
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
     val actualArrival: OffsetDateTime,
     val plannedTrack: String,
     val actualTrack: String,
--- timetable/src/main/kotlin/md/vnastasi/cloud/endpoint/model/Departure.kt
+++ timetable/src/main/kotlin/md/vnastasi/cloud/endpoint/model/Departure.kt
@@ -1,10 +1,13 @@
 package md.vnastasi.cloud.endpoint.model
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.OffsetDateTime
 
 data class Departure(
     val destination: String,
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
     val plannedDeparture: OffsetDateTime,
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
     val actualDeparture: OffsetDateTime,
     val plannedTrack: String,
     val unit: TransportationUnit,
