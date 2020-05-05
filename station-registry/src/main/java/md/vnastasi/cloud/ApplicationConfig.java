ec376671af5ad0bcbd9cb8f0638c865362c291b9
Valentin Nastasi <pragmatiku@protonmail.com>
2020-05-05 11:28:26 +0200

    Cache and log stations

--- station-registry/src/main/java/md/vnastasi/cloud/ApplicationConfig.java
+++ station-registry/src/main/java/md/vnastasi/cloud/ApplicationConfig.java
@@ -1,9 +1,17 @@
 package md.vnastasi.cloud;
 
+import io.netty.bootstrap.Bootstrap;
 import lombok.NonNull;
+import md.vnastasi.cloud.log.HttpClientLogger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.channel.BootstrapHandlers;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.TcpClient;
+
+import java.util.function.Function;
 
 @Configuration
 public class ApplicationConfig {
@@ -17,6 +25,16 @@ public class ApplicationConfig {
         var headerName = applicationProperties.getNsApi().getPublicTravelInfo().getApiKey().getHeader();
         var headerValue = applicationProperties.getNsApi().getPublicTravelInfo().getApiKey().getValue();
 
-        return builder.baseUrl(url).defaultHeader(headerName, headerValue).build();
+        return builder
+                .clientConnector(new ReactorClientHttpConnector(createHttpClient()))
+                .baseUrl(url)
+                .defaultHeader(headerName, headerValue)
+                .build();
+    }
+
+    private HttpClient createHttpClient() {
+        Function<Bootstrap, Bootstrap> bootstrapFunction = bootstrap -> BootstrapHandlers.updateLogSupport(bootstrap, new HttpClientLogger(HttpClient.class));
+        Function<TcpClient, TcpClient> tcpClientFunction = tcpClient -> tcpClient.bootstrap(bootstrapFunction);
+        return HttpClient.create().tcpConfiguration(tcpClientFunction);
     }
 }
