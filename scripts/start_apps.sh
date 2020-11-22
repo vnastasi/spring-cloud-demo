f9edd0b24c32b910cce2eb09bfb7f829f2a6743e
vnastasi <pragmatiku@protonmail.com>
2020-11-22 18:02:49 +0100

    Use jenv to set Java version

--- scripts/start_apps.sh
+++ scripts/start_apps.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 shopt -s expand_aliases
 
-alias java11="/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home/bin/java"
+jenv local 11
 
 echo "Starting Eureka Server..."
-java11 -jar eureka-server-*.jar &
+java -jar eureka-server-*.jar &
 sleep 30s
 
 echo "Starting Config Server..."
-java11 -jar config-server-*.jar &
+java -jar config-server-*.jar &
 sleep 30s
 
 echo "Starting Station Registry app..."
-java11 -jar station-registry-*.jar &
+java -jar station-registry-*.jar &
 sleep 5s
 
 echo "Starting Timetable app..."
-java11 -jar timetable-*.jar &
+java -jar timetable-*.jar &
+sleep 5s
+
+echo "Starting Disruptions app..."
+java -jar disruptions-*.jar &
 sleep 30s
 
 echo "Starting API Gateway app..."
-java11 -jar api-gateway-*.jar &
+java -jar api-gateway-*.jar &
 sleep 20s
-
-unalias java11
 
 exit
