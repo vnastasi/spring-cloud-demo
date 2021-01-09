56531594a2f654175ebb624c3aa85b3c925e05a2
vnastasi <pragmatiku@protonmail.com>
2021-01-09 19:05:48 +0100

    Upgraded scripts

--- scripts/start_apps.sh
+++ scripts/start_apps.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 shopt -s expand_aliases
 
-jenv local 11
+jenv local 15
 
 echo "Starting Eureka Server..."
 java -jar eureka-server-*.jar &
-sleep 30s
+sleep 15s
 
 echo "Starting Config Server..."
 java -jar config-server-*.jar &
-sleep 30s
+sleep 15s
 
 echo "Starting Station Registry app..."
 java -jar station-registry-*.jar &
-sleep 5s
+sleep 15s
 
 echo "Starting Timetable app..."
 java -jar timetable-*.jar &
-sleep 5s
+sleep 15s
 
 echo "Starting Disruptions app..."
 java -jar disruptions-*.jar &
-sleep 30s
+sleep 15s
 
 echo "Starting API Gateway app..."
 java -jar api-gateway-*.jar &
-sleep 20s
+sleep 30s
 
 exit
